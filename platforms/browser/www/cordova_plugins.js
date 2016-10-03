bf6f1291a4833df87b91c16cce0549e933118bd6
Hasibullah Sahibzada <hasibullah@sahibzada.org>
2016-10-03 19:11:41 +0200

    archive folders extracted

---
+++ platforms/browser/www/cordova_plugins.js
@@ -0,0 +1,19 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/com.telerik.plugins.nativepagetransitions/www/NativePageTransitions.js",
+        "id": "com.telerik.plugins.nativepagetransitions.NativePageTransitions",
+        "pluginId": "com.telerik.plugins.nativepagetransitions",
+        "clobbers": [
+            "window.plugins.nativepagetransitions"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "com.telerik.plugins.nativepagetransitions": "0.6.5",
+    "cordova-plugin-crosswalk-webview": "2.0.0"
+}
+// BOTTOM OF METADATA
+});
